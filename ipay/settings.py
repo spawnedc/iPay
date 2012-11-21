84efbf41fedfbe336d2f0b574ffd12f6e7806a34
Can Yilmaz <can@potatolondon.com>
2012-11-21 00:45:03 +0000

    Adding sqlite db for local

--- ipay/settings.py
+++ ipay/settings.py
@@ -11,8 +11,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'iPayData',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -140,8 +140,9 @@ LOGGING = {
     }
 }
 
-import dj_database_url
-DATABASES['default'] = dj_database_url.config(default='postgres://postgres:fokbalik@localhost:5432/ipay')
+if not DEBUG:
+    import dj_database_url
+    DATABASES['default'] = dj_database_url.config(default='postgres://postgres:fokbalik@localhost:5432/ipay')
 
 
 # Custom configuration
